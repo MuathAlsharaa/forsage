554bb238586863b6b67f41bb08c328cce72709b1
MuathAlsharaa <112206382+MuathAlsharaa@users.noreply.github.com>
2023-05-11 15:33:37 +0400

    first commit

---
+++ forsage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForsageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'forsage'
---
+++ forsage/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import re
+
+
+# Create your views here.
+
+
+
+
+def fahed(request):
+    return render (request , 'for/fahed.html')
+
